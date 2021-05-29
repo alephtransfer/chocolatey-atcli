93f33215adadf4c5180dbf67886133e1f55e08e2
alephtransfer <84237286+alephtransfer@users.noreply.github.com>
2021-05-29 19:25:57 +0200

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -10,9 +10,9 @@ $packageArgs = @{
 
   softwareName  = 'atcli*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum      = '64571E22AD2BF32B2BEFEFD554A63A58A092D77FA3803DAAFDB2D99F8DB518FF'
+  checksum      = '8f2607eb9985bb0344ad6827235601baaf04bbf57dc49c2a12c1819ef6fede7c'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = '64571E22AD2BF32B2BEFEFD554A63A58A092D77FA3803DAAFDB2D99F8DB518FF'
+  checksum64    = '8f2607eb9985bb0344ad6827235601baaf04bbf57dc49c2a12c1819ef6fede7c'
   checksumType64= 'sha256' #default is md5, can also be sha1, sha256 or sha512
 
   # MSI
